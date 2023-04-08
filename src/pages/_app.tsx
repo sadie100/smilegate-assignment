ed7a8ad4fd6a27d50d81b00928a021fb25dcca1e
김세희 <sadie1001@naver.com>
2023-04-08 19:17:11 +0900

    fix : Modal 로직을 공통 App 파일로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ModalProvider } from "@/contexts/modalContext";
+import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ModalProvider>
+        <Component {...pageProps} />
+        <Modal />
+      </ModalProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-import Modal from "@/components/Modal";
-import { ModalProvider } from "@/contexts/modalContext";
+import Button from "@/components/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,23 +10,13 @@ const Home = () => {
     router.push("/coupons/advance-reservation");
   };
   return (
-    <ModalProvider>
-      <main className="flex min-h-screen justify-center flex-col items-center gap-10 p-24 bg-white">
-        <button
-          className="w-[80%] min-w-[200px] rounded-md bg-main-600 px-6 py-4 text-sm font-semibold text-white shadow-sm hover:bg-main-500 text-lg"
-          onClick={handleAdvanceReserve}
-        >
-          게임 사전 예약 쿠폰 발급 페이지
-        </button>
-        <button
-          className="w-[80%] min-w-[200px] rounded-md bg-main-600 px-6 py-4 text-sm font-semibold text-white shadow-sm hover:bg-main-500 text-lg"
-          onClick={handleAdvanceReserve}
-        >
-          쿠폰 관리 페이지
-        </button>
-        <Modal />
-      </main>
-    </ModalProvider>
+    <main className="flex min-h-screen justify-center flex-col items-center gap-10 p-24 bg-white">
+      <Button
+        text="게임 사전 예약 쿠폰 발급 페이지"
+        handleClick={handleAdvanceReserve}
+      />
+      <Button text="쿠폰 관리 페이지" handleClick={handleAdvanceReserve} />
+    </main>
   );
 };
 
