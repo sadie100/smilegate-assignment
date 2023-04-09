94b44bb0f4299be1a6bd02bf12f5799a741d66af
김세희 <sadie1001@naver.com>
2023-04-09 20:31:00 +0900

    chore : types 파일 내로 옮김

--- client/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { FieldValues, RegisterOptions } from "react-hook-form";
-
-export type FormItemType = {
-  label?: string;
-  type: string;
-  name: string;
-  validation: RegisterOptions<FieldValues, string> | undefined;
-  errorMessage?: string;
-};
