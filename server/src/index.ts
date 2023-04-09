68bfe4620bbf05076e1de09d34208830db0bbd5b
김세희 <sadie1001@naver.com>
2023-04-09 22:55:17 +0900

    chore : db 연결

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,22 @@ import express, { Express, Request, Response } from "express";
 import couponRouter from "./routes/couponRouter";
 import cors from "cors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 const port = "8000";
 
 const app: Express = express();
 app.use(cors());
 app.use(json());
+
+//db connect
+const db = "mongodb://127.0.0.1:27017";
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("DB 연결 완료");
+  })
+  .catch((err) => console.log(err));
 
 app.use("/api", couponRouter);
 
