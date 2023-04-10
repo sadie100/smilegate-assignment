5a1baac5aec890ce7f176b5d1f31a831bfff1bc4
김세희 <sadie1001@naver.com>
2023-04-10 15:54:44 +0900

    Fix : 쿠폰 모델에 timestamp 옵션 추가

--- server/src/models/Coupon.ts
+++ server/src/models/Coupon.ts
@@ -6,18 +6,23 @@ interface ICoupon extends Document {
   couponId: string;
 }
 
-const CouponSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  phone: {
-    type: String,
-    required: true,
-    get: (data: string) =>
-      data
-        .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
-        .replace(/(\-{1,2})$/g, ""),
-    set: (data: string) => data.replaceAll("-", ""),
+const CouponSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+      get: (data: string) =>
+        data
+          .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
+          .replace(/(\-{1,2})$/g, ""),
+      set: (data: string) => data.replaceAll("-", ""),
+    },
+    couponId: { type: String, required: true },
   },
-  couponId: { type: String, required: true },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default model<ICoupon>("Coupon", CouponSchema);
