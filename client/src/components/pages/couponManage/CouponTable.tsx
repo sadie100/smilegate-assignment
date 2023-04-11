70c6c0c6ae5826c228856f3a7acbe6307d9caf54
김세희 <sadie1001@naver.com>
2023-04-12 02:58:57 +0900

    Fix : 테이블 ui 수정

--- client/src/components/pages/couponManage/CouponTable.tsx
+++ client/src/components/pages/couponManage/CouponTable.tsx
@@ -15,7 +15,7 @@ const CouponTable = () => {
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 text-center">
             <tr>
               <th scope="col" className="px-6 py-3">
                 이름
@@ -36,7 +36,7 @@ const CouponTable = () => {
               return (
                 <tr
                   key={idx}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center"
                 >
                   <td className="px-6 py-4">{name}</td>
                   <td className="px-6 py-4">{phone}</td>
